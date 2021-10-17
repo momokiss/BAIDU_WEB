d1e38f6bd3271e41ac876a79b8f7fdceda2b3d36
momokiss <102050933+momokiss@users.noreply.github.com>
2021-10-17 14:05:16 +0000

    few small changes

--- src/components/base/Footer.vue
+++ src/components/base/Footer.vue
@@ -1,3 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-import { computed, onBeforeUnmount, onMounted, reactive } f
+import { computed, onBeforeUnmount, onMounted, reactive } from "vue"
+import { useRouter } from "vue-router"
+import axios from "axios"
+import { DateTime } from "luxon"
+
+/**
+ * Services
+ 
