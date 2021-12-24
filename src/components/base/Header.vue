a2a6a564dc206f7da75a517e87ed2fcc08a93f54
momokiss <102050933+momokiss@users.noreply.github.com>
2021-12-24 10:13:45 +0000

    jj

--- src/components/base/Header.vue
+++ src/components/base/Header.vue
@@ -156,3 +156,121 @@ const handleDisagree = () => {
 	showConnectingModal.value = false
 
 	address.value = ""
+
+	juster.sdk._provider.client.clearActiveAccount().then(async () => {
+		await juster.sdk._provider.client.getActiveAccount()
+		accountStore.setPkh("")
+		router.push("/")
+	})
+}
+
+const handleOpenProfile = () => {
+	router.push("/profile")
+
+	analytics.log("openProfile")
+}
+
+const handleOpenWithdrawals = () => {
+	router.push("/withdrawals")
+
+	analytics.log("openWithdrawals")
+}
+
+const handleLogout = () => {
+	juster.sdk._provider.client.clearActiveAccount().then(async () => {
+		await juster.sdk._provider.client.getActiveAccount()
+
+		analytics.log("logout", { address: accountStore.pkh })
+
+		accountStore.setPkh("")
+		router.push("/")
+
+		accountStore.positionsForWithdrawal = []
+
+		notificationsStore.create({
+			notification: {
+				type: "success",
+				title: "You are signed out",
+				description:
+					"To work with the application, you definitely need an account :)",
+				autoDestroy: true,
+			},
+		})
+	})
+}
+
+const pkh = computed(() => accountStore.pkh)
+</script>
+
+<template>
+	<header :class="$style.wrapper">
+		<CustomLoginModal :show="showCustomLoginModal" @onSelectCustomNode="handleSelectCustomNode"
+			@onClose="showCustomLoginModal = false" />
+		<ConnectingModal :show="showConnectingModal" :address="address" @onAgree="handleAgree" @onClose="handleDisagree" />
+
+		<!-- Mobile menu -->
+		<transition name="fade">
+			<div v-if="showMobileMenu" @click="showMobileMenu = false" :class="$style.mobile_menu">
+				<div :class="$style.mobile_menu__title">Navigation</div>
+
+				<div :class="$style.mobile_menu__links">
+					<router-link to="/" :class="$style.mobile_menu__link">
+						<div :class="$style.left">
+							<Icon name="bolt" size="14" />
+							<span>Explore</span>
+						</div>
+
+						<div :class="$style.mobile_menu__description">
+							Top markets, ranking, etc
+						</div>
+					</router-link>
+					<router-link to="/events" :class="$style.mobile_menu__link">
+						<div :class="$style.left">
+							<Icon name="arrows" size="14" />
+							<span>Events</span>
+						</div>
+
+						<div :class="$style.mobile_menu__description">
+							Events with filtering
+						</div>
+					</router-link>
+					<router-link to="/markets" :class="$style.mobile_menu__link">
+						<div :class="$style.left">
+							<Icon name="collection" size="14" />
+							<span>Markets</span>
+						</div>
+
+						<div :class="$style.mobile_menu__description">
+							Currency pairs for events
+						</div>
+					</router-link>
+				</div>
+			</div>
+		</transition>
+
+		<div :class="$style.base">
+			<div :class="$style.left">
+				<div @click="showMobileMenu = !showMobileMenu" :class="$style.mobile_menu_icon">
+					<Icon :name="showMobileMenu ? 'close' : 'menu'" size="16" />
+				</div>
+
+				<router-link to="/" :class="$style.logo">
+					<Icon name="logo_symbol" size="24" />
+					<img src="@/assets/icons/logo_text.svg" alt="logo" />
+				</router-link>
+			</div>
+
+			<div :class="$style.links">
+				<router-link v-for="link in links" :key="link.name" :to="link.url" :class="[
+					$style.link,
+					isActive(link.url) && $style.active,
+					!link.isAvailable && $style.disabled,
+				]">
+					<Icon :name="link.icon" size="12" />{{ link.name }}
+				</router-link>
+			</div>
+
+			<div :class="$style.right">
+				<Tooltip v-if="currentNetwork !== Networks.MAINNET" side="right">
+					<div :class="$style.testnet_warning">
+						<Icon name="hammer" size="16" />
