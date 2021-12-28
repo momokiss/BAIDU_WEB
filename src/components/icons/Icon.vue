5b19ebfabc72c67c0ce797bcffc7d6e2902f2188
momokiss <102050933+momokiss@users.noreply.github.com>
2021-12-28 17:50:44 +0000

    Update deploy

---
+++ src/components/icons/Icon.vue
@@ -0,0 +1,32 @@
+
+<script>
+import icons from "./icons.json"
+
+export default {
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: "Icon",
+	props: {
+		name: { type: String, required: true },
+		size: String,
+		color: String,
+	},
+	computed: {
+		calcSize() {
+			return {
+				minWidth: `${this.size}px`,
+				minHeight: `${this.size}px`,
+			}
+		},
+	},
+	methods: {
+		getPath() {
+			return icons[this.name.charAt(0).toLowerCase() + this.name.slice(1)]
+		},
+		isSplitted() {
+			return (
+				typeof icons[
+				this.name.charAt(0).toLowerCase() + this.name.slice(1)
+				] == "object"
+			)
+		},
+	},
