85bed2927dfd5f8f750243147595af1789d32e69
momokiss <102050933+momokiss@users.noreply.github.com>
2022-01-13 06:23:28 +0000

    Fix gcc warning.

--- src/components/local/MarketCard.vue
+++ src/components/local/MarketCard.vue
@@ -1,3 +1,14 @@
 <!-- eslint-disable vue/no-unused-components -->
 <script>
-import { defineComponent, toRefs, onMounted, computed, watch, re
+import { defineComponent, toRefs, onMounted, computed, watch, ref } from "vue"
+import * as d3 from "d3"
+
+/**
+ * UI
+ */
+import Button from "@/components/ui/Button"
+
+/**
+ * Local
+ */
+import MarketStatu
