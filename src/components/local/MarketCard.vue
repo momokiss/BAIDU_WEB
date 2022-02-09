9e0b629251387430f84e31e76ba8b5857f27cd02
momokiss <102050933+momokiss@users.noreply.github.com>
2022-02-09 11:51:31 +0000

    Update aula.md

--- src/components/local/MarketCard.vue
+++ src/components/local/MarketCard.vue
@@ -82,4 +82,8 @@ export default defineComponent({
         .select(`#chart_${market.value.id}`)
         .append("svg")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`)
+
+      const data = prepareQuotesForD3({ quot
