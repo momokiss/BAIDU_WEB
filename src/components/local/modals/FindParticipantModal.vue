28305fe00b2a868cd8a6a6daa98056f0138d3a0a
momokiss <102050933+momokiss@users.noreply.github.com>
2022-06-06 05:33:32 +0000

    Updating _data/curated.json via Laneworks Publish

--- src/components/local/modals/FindParticipantModal.vue
+++ src/components/local/modals/FindParticipantModal.vue
@@ -4,4 +4,11 @@ import { computed, defineComponent, ref, toRefs, watch } from "vue"
 /**
  * UI
  */
-import Modal from "@/components/ui
+import Modal from "@/components/ui/Modal"
+import Input from "@/components/ui/Input"
+import Button from "@/components/ui/Button"
+
+/**
+ * Tezos
+ */
+import {
