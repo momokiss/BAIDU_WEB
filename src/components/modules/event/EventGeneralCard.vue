fb1628459ae465b072bba0fb7e4e73a67db80778
momokiss <102050933+momokiss@users.noreply.github.com>
2022-07-05 06:41:32 +0000

    bump to v0.0.2

--- src/components/modules/event/EventGeneralCard.vue
+++ src/components/modules/event/EventGeneralCard.vue
@@ -69,3 +69,125 @@ const participantsAvatars = computed(() => {
 
 	return avatars
 })
+
+const timing = computed(() => {
+	const eventDt = DateTime.fromISO(props.event.betsCloseTime).setLocale("en")
+
+	const endDt = eventDt.plus(props.event.measurePeriod * 1000)
+
+	return {
+		start: {
+			time: eventDt.toLocaleString({
+				hour: "numeric",
+				minute: "numeric",
+			}),
+			date: eventDt.toLocaleString({
+				day: "numeric",
+				month: "short",
+			}),
+			day: eventDt.toFormat("cccc"),
+		},
+		end: {
+			time: endDt.toLocaleString({
+				hour: "numeric",
+				minute: "numeric",
+			}),
+			date: endDt.toLocaleString({
+				day: "numeric",
+				month: "short",
+			}),
+			day: endDt.toFormat("cccc"),
+		},
+		showDay: eventDt.ordinal < endDt.ordinal,
+	}
+})
+
+const priceDynamics = computed(() => {
+	const startRate = props.event.startRate * 100
+	const closedRate = props.event.closedRate * 100
+
+	const percent =
+		props.event.status == "FINISHED"
+			? (100 * Math.abs(closedRate - startRate)) /
+			((closedRate + startRate) / 2)
+			: (100 * Math.abs(props.price.rate - startRate)) /
+			((props.price.rate + startRate) / 2)
+
+	const diff =
+		props.event.status == "FINISHED"
+			? closedRate - startRate
+			: props.price.rate - startRate
+
+	return { diff, percent }
+})
+
+const endDiff = computed(() =>
+	DateTime.fromISO(props.event.betsCloseTime)
+		.plus({
+			second: props.event.measurePeriod,
+		})
+		.diff(DateTime.fromISO(props.event.betsCloseTime), ["hours"])
+		.toObject(),
+)
+</script>
+
+<template>
+	<div :class="$style.wrapper">
+		<div :class="$style.header">
+			<div :class="$style.title">
+				<img v-if="event.winnerBets == 'ABOVE_EQ'" :src="require('@/assets/icons/higher_won.svg')"
+					alt="won_side_icon" />
+				<img v-else-if="event.winnerBets == 'BELOW'" :src="require('@/assets/icons/lower_won.svg')"
+					alt="won_side_icon" />
+				<Icon v-else name="sides" size="16" />
+				{{
+						supportedMarkets[symbol] && supportedMarkets[symbol].target
+				}}
+				<span>price event</span>
+			</div>
+
+			<div :class="$style.users">
+				<Tooltip position="bottom" side="right">
+					<div @click="emit('openParticipants')" :class="$style.participants">
+						<img v-for="participantAvatar in participantsAvatars.slice(
+							0,
+							3,
+						)" :key="participantAvatar" :src="`https://services.tzkt.io/v1/avatars/${participantAvatar}`"
+							:class="[$style.user_avatar, $style.participant]" alt="avatar" />
+						<div v-if="participantsAvatars.length > 3" :class="[
+							$style.participant,
+							$style.more_participants,
+						]">
+							+{{ participantsAvatars.length - 3 }}
+						</div>
+					</div>
+
+					<template v-slot:content>Participants ({{
+							participantsAvatars.length
+					}})</template>
+				</Tooltip>
+
+				<Tooltip position="bottom" side="right">
+					<div :class="$style.creator">
+						<template v-if="
+							verifiedMakers[currentNetwork].includes(
+								event.creatorId,
+							)
+						">
+							<Icon name="logo_symbol" size="24" />
+							<Icon name="verified" size="16" :class="$style.verified_icon" />
+						</template>
+
+						<template v-else>
+							<img :src="`https://services.tzkt.io/v1/avatars/${event.creatorId}`" :class="$style.user_avatar"
+								alt="avatar" />
+						</template>
+					</div>
+
+					<template v-slot:content>{{
+							verifiedMakers[currentNetwork].includes(event.creatorId)
+								? "Recurring event from Juster"
+								: "Custom event from user"
+					}}</template>
+				</Tooltip>
+			</div>
