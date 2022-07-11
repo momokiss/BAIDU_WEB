37737c5ed3fd9b26bac20c01c19bd1a175afa2ba
momokiss <102050933+momokiss@users.noreply.github.com>
2022-07-11 19:36:18 +0000

    jslint fix

--- src/components/modules/explore/ExploreBase.vue
+++ src/components/modules/explore/ExploreBase.vue
@@ -30,3 +30,125 @@ import Button from "@/components/ui/Button"
  */
 import { fetchTopEvents } from "@/api/events"
 import { fetchTopBettors, fetchTopLiquidityProviders } from "@/api/users"
+
+/**
+ * Subscriptions
+ */
+import { eventSub } from "@/graphql/subscriptions/event"
+
+/**
+ * Store
+ */
+import { useMarketStore } from "@/store/market"
+import { useAccountStore } from "@/store/account"
+
+/**
+ * Services
+ */
+import { juster, analytics } from "@/services/sdk"
+
+export default defineComponent({
+  name: "ExploreBase",
+
+  setup() {
+    const router = useRouter()
+
+    const marketStore = useMarketStore()
+    const accountStore = useAccountStore()
+
+    const subToMyPositions = ref({})
+    const myPositions = ref([])
+
+    /** Ranking */
+    const isTopProvidersLoading = ref(true)
+    const isTopBettorsLoading = ref(true)
+    const topProviders = ref([])
+    const topBettors = ref([])
+
+    const handleViewTopEvents = () => {
+      router.push("/events/top")
+    }
+
+    onMounted(async () => {
+      analytics.log("onPage", { name: "Explore" })
+
+      /**
+       * Block: My Positions
+       */
+      subToMyPositions.value = await juster.gql
+        .subscription({
+          position: [
+            {
+              where: {
+                userId: { _eq: accountStore.pkh },
+                event: { status: { _in: ["NEW", "STARTED"] } },
+              },
+            },
+            {
+              id: true,
+              value: true,
+              event: eventSub,
+            },
+          ],
+        })
+        .subscribe({
+          next: ({ position: positions }) => {
+            myPositions.value = positions
+          },
+          error: console.error,
+        })
+
+      /**
+       * Block: Top Events & Providers
+       */
+      const topEvents = await fetchTopEvents({ limit: 3 })
+      marketStore.events = cloneDeep(topEvents).sort(
+        (a, b) => b.bets.length - a.bets.length,
+      )
+
+      const rawTopProviders = await fetchTopLiquidityProviders()
+      const rawTopBettors = await fetchTopBettors()
+
+      topProviders.value = rawTopProviders.map((el) => {
+        return { address: el.address, value: el.totalProviderReward }
+      })
+      isTopProvidersLoading.value = false
+
+      topBettors.value = rawTopBettors.map((el) => {
+        return { address: el.address, value: el.totalBetsCount }
+      })
+      isTopBettorsLoading.value = false
+    })
+    onBeforeUnmount(() => {
+      marketStore.events = []
+    })
+
+    onUnmounted(() => {
+      if (
+        subToMyPositions.value.hasOwnProperty("_state") &&
+        !subToMyPositions.value?.closed
+      ) {
+        subToMyPositions.value.unsubscribe()
+      }
+    })
+
+    /** Meta */
+    useMeta({
+      title: "Explore",
+      description:
+        "Juster is an on-chain smart contract platform allowing users to take part in an automated betting market by creating events, providing liquidity to them, and making bets.",
+    })
+
+    return {
+      marketStore,
+      myPositions,
+      topProviders,
+      topBettors,
+      isTopBettorsLoading,
+      isTopProvidersLoading,
+      handleViewTopEvents,
+    }
+  },
+
+  components: {
+    RatingCard,
