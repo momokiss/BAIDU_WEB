4428eb66dfa91bfcb799397c6d42d5588bbc7c27
momokiss <102050933+momokiss@users.noreply.github.com>
2022-07-11 22:54:36 +0000

    Improve javadoc.

--- src/components/modules/explore/ExploreBase.vue
+++ src/components/modules/explore/ExploreBase.vue
@@ -152,3 +152,126 @@ export default defineComponent({
 
   components: {
     RatingCard,
+    RatingCardLoading,
+    MyPositionCard,
+    MarketCard,
+    EventCard,
+    EventCardLoading,
+    Button,
+  },
+})
+</script>
+
+<template>
+  <transition name="fade">
+    <div :class="$style.wrapper">
+      <metainfo>
+        <template v-slot:title="{ content }">{{ content }} • Juster</template>
+      </metainfo>
+
+      <!-- My Positions -->
+      <div v-if="myPositions.length" :class="$style.block">
+        <h1>My Positions</h1>
+        <div :class="$style.description">
+          Events in which you have liquidity or bet
+        </div>
+
+        <div :class="$style.my_positions">
+          <MyPositionCard v-for="position in myPositions" :key="position.id" :event="position.event" />
+        </div>
+      </div>
+
+      <!-- Top markets -->
+      <div v-if="marketStore.isMarketsLoaded" :class="$style.block">
+        <h1>Top Markets</h1>
+        <div :class="$style.description">
+          Currency pairs available for betting
+        </div>
+
+        <div :class="$style.items">
+          <MarketCard v-for="market in marketStore.markets" :key="market.id" :market="market" />
+        </div>
+      </div>
+
+      <!-- Top Liquidity -->
+      <div :class="$style.block">
+        <h1>Top Liquidity</h1>
+        <div :class="$style.description">
+          Rating of users providing liquidity
+        </div>
+
+        <RatingCard v-if="!isTopProvidersLoading" :users="topProviders" suffix="ꜩ" :class="$style.rating_card" />
+        <RatingCardLoading v-else :class="$style.rating_card" />
+      </div>
+
+      <!-- Hot events -->
+      <div :class="$style.block">
+        <div :class="$style.head">
+          <div :class="$style.left">
+            <h1>Hot Events</h1>
+            <div :class="$style.description">
+              Events that have not yet begun, but are attracting
+              the interest of participants
+            </div>
+          </div>
+
+          <Button @click="handleViewTopEvents" size="small" type="secondary">
+            <Icon name="collection" size="16" />View all
+          </Button>
+        </div>
+
+        <transition name="fastfade" mode="out-in">
+          <div v-if="marketStore.events.length" :class="$style.items">
+            <EventCard v-for="event in marketStore.events" :key="event.id" :event="event" />
+          </div>
+
+          <div v-else :class="$style.items">
+            <EventCardLoading />
+            <EventCardLoading />
+            <EventCardLoading />
+          </div>
+        </transition>
+      </div>
+
+      <!-- Top Bettors -->
+      <div :class="$style.block">
+        <h1>Top Bettors</h1>
+        <div :class="$style.description">
+          Rating of users placing bets
+        </div>
+
+        <RatingCard v-if="!isTopBettorsLoading" :users="topBettors" suffix="Bets" :class="$style.rating_card" />
+        <RatingCardLoading v-else :class="$style.rating_card" />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style module>
+.wrapper {}
+
+.block {
+  margin-bottom: 60px;
+}
+
+.block:last-child {
+  margin-bottom: 0;
+}
+
+.block h1 {
+  letter-spacing: 0.5px;
+}
+
+.head {
+  display: flex;
+  justify-content: space-between;
+}
+
+.left {
+  margin-right: 40px;
+}
+
+.description {
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 22px;
