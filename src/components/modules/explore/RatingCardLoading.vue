45ff52d822175c6ecddfbb9bf7e293234fb2b3af
momokiss <102050933+momokiss@users.noreply.github.com>
2022-08-19 11:20:13 +0000

    Cleaning up a bit

--- src/components/modules/explore/RatingCardLoading.vue
+++ src/components/modules/explore/RatingCardLoading.vue
@@ -3,4 +3,9 @@
 
 <template>
   <div :class="$style.wrapper">
-    <div v-for="index in 5" :key
+    <div v-for="index in 5" :key="index" :class="$style.user">
+      <div :class="$style.position" />
+
+      <div :class="$style.avatar" />
+
+      <div :class=
