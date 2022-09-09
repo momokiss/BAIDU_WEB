e21e03f378721452a517f80ac9a83f1ab285f3f6
momokiss <102050933+momokiss@users.noreply.github.com>
2022-09-09 07:48:49 +0000

    Fixed validation

--- src/components/modules/symbols/Market.vue
+++ src/components/modules/symbols/Market.vue
@@ -1,2 +1,13 @@
 <script>
-import { defineComponent, ref, toRefs
+import { defineComponent, ref, toRefs, computed } from "vue"
+
+/**
+ * UI
+ */
+import Button from "@/components/ui/Button"
+
+/**
+ * Services
+ */
+import {
+  numberWithSymbol,
