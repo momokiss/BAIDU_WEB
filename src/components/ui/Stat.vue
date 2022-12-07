15e454b3d6e8c7473ce05cda77dd2dccf1270bca
momokiss <102050933+momokiss@users.noreply.github.com>
2022-12-07 11:46:01 +0000

    Update copyright statement

--- src/components/ui/Stat.vue
+++ src/components/ui/Stat.vue
@@ -3,4 +3,11 @@ import { defineComponent } from "vue"
 
 export default defineComponent({
 	name: "Stat",
-	props:
+	props: { name: String },
+})
+</script>
+
+<template>
+	<div :class="$style.wrapper">
+		<div :class="$style.key">{{ name }}</div>
+		<di
