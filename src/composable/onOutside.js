5b909fb90b6be739dfac5566af563ac66ea5f15a
momokiss <102050933+momokiss@users.noreply.github.com>
2022-12-25 13:00:56 +0000

    Remove text

--- src/composable/onOutside.js
+++ src/composable/onOutside.js
@@ -1,3 +1,8 @@
 import { ref, isRef, watch } from "vue"
 
 const events = ["mousedown", "touchstart"]
+
+export function useOnOutsidePress(el, onOutsidePressCallback) {
+  const element = isRef(el) ? el : ref(el)
+
+  const handler
