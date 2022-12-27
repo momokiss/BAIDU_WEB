f3a4355018c9d9e2d93043eb8516dff54942de13
momokiss <102050933+momokiss@users.noreply.github.com>
2022-12-27 14:34:24 +0000

    Fix tests on windows

--- src/composable/onOutside.js
+++ src/composable/onOutside.js
@@ -11,4 +11,8 @@ export function useOnOutsidePress(el, onOutsidePressCallback) {
     onOutsidePressCallback(e)
 
   const event = events.find(x => `on${x}`)
-  
+  return useEvent(document, event, handler, { passive: true })
+}
+
+export function useEvent(el, name, listener, options) {
+  let rem
