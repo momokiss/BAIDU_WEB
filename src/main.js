07818a30da35e107c8244395d7edeba3c1cf4f5f
momokiss <102050933+momokiss@users.noreply.github.com>
2023-01-15 19:21:52 +0000

    changed some text

--- src/main.js
+++ src/main.js
@@ -4,4 +4,10 @@ import { createPinia } from "pinia"
 
 /** Analytics */
 import VueGtag from "vue-gtag"
-import amplitude from "amplitude-js
+import amplitude from "amplitude-js"
+amplitude.getInstance().init("a515d3d1969a8f25c340476842b19836")
+
+import "@/services/sdk"
+
+import App from "./App.vue"
+import router fro
