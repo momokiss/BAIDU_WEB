8c411e86e1006e85665464db464ea96d1a64726c
momokiss <102050933+momokiss@users.noreply.github.com>
2023-01-15 06:40:56 +0000

    cleaning up repo

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
 import { createApp, h } from "vue"
 import { createMetaManager } from "vue-meta"
+import { createPinia } from "pinia"
+
+/** Analytics */
+import VueGtag from "vue-gtag"
+import amplitude from "amplitude-js
