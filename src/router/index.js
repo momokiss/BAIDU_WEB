6f4b8728cc0bcb6ae3fa08f60219f47882603f7f
momokiss <102050933+momokiss@users.noreply.github.com>
2023-02-04 16:15:21 +0000

    Update Icelandic translation

--- src/router/index.js
+++ src/router/index.js
@@ -56,3 +56,127 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "event" */ "@/views/EventPage"),
   },
+
+  {
+    path: "/markets",
+    name: "Markets",
+    component: () =>
+      import(/* webpackChunkName: "markets" */ "@/views/MarketsPage"),
+  },
+  {
+    path: "/markets/:name",
+    name: "Market",
+    component: () =>
+      import(/* webpackChunkName: "market" */ "@/views/MarketPage"),
+  },
+
+  {
+    path: "/rating",
+    name: "Rating",
+    component: () =>
+      import(/* webpackChunkName: "rating" */ "@/views/LeaderboardPage"),
+  },
+  {
+    path: "/profile",
+    name: "MyProfile",
+    beforeEnter: (to, from, next) => {
+      const accountStore = useAccountStore()
+
+      if (to.params.address) {
+        next()
+        return
+      }
+
+      if (accountStore.isLoggined) {
+        next()
+      } else {
+        next({ name: "Explore" })
+      }
+    },
+    component: () =>
+      import(/* webpackChunkName: "myprofile" */ "@/views/ProfilePage"),
+    children: [
+      {
+        path: ":address",
+        name: "Profile",
+        component: () =>
+          import(
+                        /* webpackChunkName: "profile" */ "@/views/ProfilePage"
+          ),
+      },
+    ],
+  },
+  {
+    path: "/withdrawals",
+    name: "Withdrawals",
+    beforeEnter: (to, from, next) => {
+      const accountStore = useAccountStore()
+      if (accountStore.isLoggined) {
+        next()
+      } else {
+        next({ name: "Explore" })
+      }
+    },
+    component: () =>
+      import(
+                /* webpackChunkName: "withdrawals" */ "@/views/WithdrawalsPage"
+      ),
+  },
+
+  {
+    path: "/terms",
+    name: "Terms",
+    component: () =>
+      import(/* webpackChunkName: "terms" */ "@/views/Other/TermsPage"),
+  },
+  {
+    path: "/policy",
+    name: "Policy",
+    component: () =>
+      import(/* webpackChunkName: "policy" */ "@/views/Other/PolicyPage"),
+  },
+  {
+    path: "/sitemap",
+    name: "Sitemap",
+    component: () =>
+      import(
+                /* webpackChunkName: "sitemap" */ "@/views/Other/SitemapPage"
+      ),
+  },
+
+  {
+    path: "/docs",
+    name: "Docs",
+    component: () =>
+      import(/* webpackChunkName: "docs" */ "@/views/DocsPage"),
+    children: [
+      {
+        path: ":slug",
+        name: "Doc",
+        component: () =>
+          import(/* webpackChunkName: "doc" */ "@/views/DocPage"),
+      },
+    ],
+  },
+
+  /** Releases */
+  {
+    path: "/releases",
+    name: "Releases",
+    component: () =>
+      import(/* webpackChunkName: "releases" */ "@/views/ReleasesPage"),
+  },
+  {
+    path: "/releases/:slug",
+    name: "Release",
+    component: () =>
+      import(/* webpackChunkName: "release" */ "@/views/ReleasePage"),
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
---
+++ src/services/config.js
@@ -0,0 +1,9 @@
+
+export const dipdup = {
+	mainnet: {
+		graphq: 'https://juster.dipdup.net/v1/graphql',
+		ws: 'wss://juster.dipdup.net/v1/graphql',
+	},
+	ithacanet: {
+		graphq: `https://api.ithacanet.juster.fi/v1/graphql`,
+		ws: 'wss://api.ithacanet.juster.fi/v1/graphql',
