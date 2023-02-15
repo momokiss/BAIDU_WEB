7cb43d8c91c550091e35216ced1bb05482a92696
momokiss <102050933+momokiss@users.noreply.github.com>
2023-02-15 06:42:59 +0000

    updated image

--- src/services/sdk/withdraw.js
+++ src/services/sdk/withdraw.js
@@ -4,4 +4,8 @@ import { OpKind } from "@taquito/taquito"
  * Services
  */
 import { juster } from "@/services/sdk"
-import { contracts } from "@/services/con
+import { contracts } from "@/services/config"
+
+export const withdrawAll = async ({ eventIds, address }) => {
+  try {
+    const contract = await juster.sdk._tezos.contract.a
