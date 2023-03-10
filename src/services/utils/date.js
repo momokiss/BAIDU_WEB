08483eb8935401776513efd90106728d76786beb
momokiss <102050933+momokiss@users.noreply.github.com>
2023-03-10 16:43:31 +0000

    Added new translations

--- src/services/utils/date.js
+++ src/services/utils/date.js
@@ -41,3 +41,7 @@ export const toReadableDuration = ({ seconds, asObject }) => {
       return {
         val: dur.as("hours"),
         text: "hour",
+      }
+    }
+  }
+}
---
+++ src/store/account.js
@@ -0,0 +1,60 @@
+
+import { defineStore } from "pinia"
+
+/**
+ * Services
+ */
+import { fetchBalance } from "@/services/sdk"
+
+export const useAccountStore = defineStore({
+  id: "account",
+
+  state() {
+    return {
+      pkh: "",
+      balance: 0,
+
+      pendingTransaction: {
+        awaiting: false,
+        when: null,
+      },
+
+      isPositionsLoading: false,
+      positionsForWithdrawal: [],
+
+      withdrawals: [],
+
+      showOnboarding: false,
+    }
+  },
+  actions: {
+    setPkh(pkh) {
+      this.pkh = pkh
+    },
+
+    async updateBalance() {
+      this.balance = await fetchBalance(this.pkh)
+    },
+
+    /** positions */
+    removePosition(id) {
+      const positionIndex = this.positionsForWithdrawal.findIndex(
+        pos => pos.id == id,
+      )
+      if (positionIndex == -1) return
+
+      this.positionsForWithdrawal.splice(positionIndex, 1)
+    },
+  },
+  getters: {
+    isLoggined() {
+      return !!this.pkh
+    },
+
+    wonPositions() {
+      return this.positionsForWithdrawal.filter(
+        position => position.value,
+      )
+    },
+  },
+})
