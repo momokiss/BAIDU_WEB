fc03d0c2b94a61bfc610339ea14deb815c9f3674
momokiss <102050933+momokiss@users.noreply.github.com>
2023-05-15 15:45:06 +0000

    new website

--- src/views/LeaderboardPage.vue
+++ src/views/LeaderboardPage.vue
@@ -9,4 +9,13 @@ import Page from "@/components/base/Page"
 /**
  * Module
  */
-import LeaderboardBase from "@/components/modules/leaderboard/LeaderboardBas
+import LeaderboardBase from "@/components/modules/leaderboard/LeaderboardBase"
+
+export default defineComponent({
+	name: "LeaderboardPage",
+
+	components: { Page, LeaderboardBase },
+})
+</script>
+
+<templ
