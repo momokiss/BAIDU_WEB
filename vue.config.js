1a9ea795077c9b20e2f330f39a61b718d04611f9
momokiss <102050933+momokiss@users.noreply.github.com>
2023-05-17 13:04:10 +0000

    stash

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,11 @@ module.exports = {
     module: {
       rules: [
         {
-    
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto",
+        },
+      ],
+    },
+  },
+}
